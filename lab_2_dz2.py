0dceb9c34491fdf9dcbe6fa505a289fb3d65ea1c
Maxim12395 <71729685+Maxim12395@users.noreply.github.com>
2020-10-02 22:38:27 +0200

    253

--- lab_2_dz2.py
+++ lab_2_dz2.py
@@ -1,6 +1,6 @@
 a1=float(input('первый член: '))
-b=int(input('количество членов: '))
-c=float(input('знаменатель: '))
+b=float(input('знаменатель: '))
+c=int(input('количество членов: '))
 
-for a in range(1,b,1):
-    print(a1*c**(a), end='')
+for a in range(1,c,1):
+    print(a1*b**(a), end='    ')
