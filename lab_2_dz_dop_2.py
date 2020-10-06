c73e0d04b0d70bb34ce6f000e7291f57915d645b
Maxim12395 <71729685+Maxim12395@users.noreply.github.com>
2020-10-06 21:04:21 +0200

    9876

---
+++ lab_2_dz_dop_2.py
@@ -0,0 +1,15 @@
+a=int(input("a = "))
+b=int(input("b = "))
+c=int(input("c = "))
+ 
+if a+b<=c or a+c<=b or b+c<=a :
+    print("Треугольник не существует")
+    
+elif a!=b and a!=c and b!=c :
+    print("разносторонний")
+
+elif a==b==c:
+    print("равносторонний")    
+    
+else:
+    print("равнобедренный")
