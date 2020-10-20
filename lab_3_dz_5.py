af130710fe894cc2d38c61299e0982b5989ab854
Maxim12395 <71729685+Maxim12395@users.noreply.github.com>
2020-10-20 17:32:48 +0200

    3265426524357638

--- lab_3_dz_5.py
+++ lab_3_dz_5.py
@@ -1,17 +1,17 @@
 from random import random
-M = 10
-N = 5
+A = 10
+B = 5
 a = []
-for i in range(N):
+for i in range(A):
     b = []
-    for j in range(M):
+    for j in range(B):
         b.append(round(random()*2))
     a.append(b)
     print(b)
  
-c1 = int(input("Один столбец: ")) - 1
-c2 = int(input("Другой столбец: ")) - 1
+c1 = int(input("Первый столбец: ")) - 1
+c2 = int(input("Второф столбец: ")) - 1
 
-for i in range(N):
+for i in range(A):
     a[i][c1], a[i][c2] = a[i][c2], a[i][c1]
     print(a[i])
