d824c42c09e80e675a6053ec1381d77080368a3f
Maxim12395 <71729685+Maxim12395@users.noreply.github.com>
2020-10-20 17:30:57 +0200

    wewert36t8786

--- lab_3_dz_4.py
+++ lab_3_dz_4.py
@@ -1,18 +1,18 @@
 from math import sin
-N = 3
-M = 4
+A = 3
+B = 4
 mtx = []
-for i in range(N):
+for i in range(A):
     a = []
-    for j in range(M):
-        a.append(sin(N*(i+1) + M*(j+1)))
+    for j in range(B):
+        a.append(sin(A*(i+1) + B*(j+1)))
     mtx.append(a)
  
-for i in range(N):
-    for j in range(M):
+for i in range(A):
+    for j in range(B):
         if mtx[i][j] < 0:
             mtx[i][j] = 0
             print("%7.0f" % mtx[i][j], end='')
         else:
             print("%7.2f" % mtx[i][j], end='')
-    print(N,M)
+    print(A,B)
