a93aace326b135dffee2919de731f928c55a0c28
Maxim12395 <71729685+Maxim12395@users.noreply.github.com>
2020-10-28 21:49:27 +0200

    5454645647

--- lab_3_dz_4.py
+++ lab_3_dz_4.py
@@ -1,16 +1,18 @@
-from math import sin
+  
+import numpy as np
 N = 3
 M = 4
-mtx = []
-for i in range(N):
-    a = []
-    for j in range(M):
-        a.append(sin(N*(i+1) + M*(j+1)))
-    mtx.append(a)
+A = np.ndarray(shape=(N,M))
+for i in range(0,N,1):
+    for j in range(0,M,1):
+        if i > 0 and j > 0:
+            A[i,j] = (np.sin(N*i + M*j)) 
+            
+
  
 for i in range(N):
     for j in range(M):
-        if mtx[i][j] < 0:
-            mtx[i][j] = 0
+        if A[i][j] < 0:
+            A[i][j] = 0
 
-    print(mtx)
+    print(A)  
