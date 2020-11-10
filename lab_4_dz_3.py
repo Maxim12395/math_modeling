6c9ae52c2bd846e8444755cb0d9e8534de2d264b
Maxim12395 <71729685+Maxim12395@users.noreply.github.com>
2020-11-10 16:23:47 +0200

    98098989090

--- lab_4_dz_3.py
+++ lab_4_dz_3.py
@@ -1,13 +1,11 @@
 import numpy as np
 
 
-def mult_func(E):
+def mult_func(h = 65, m = 50, v = 20, g = 10):
     pass
-h = 65
-m = 50
-v = 20
-g = 10
 
-E = ((m*v**2)/2) + (m*g*h)
+    E = ((m*v**2)/2) + (m*g*h)
+    return(E)
+tmp = mult_func(2,5,8)
 
-print(E)
+print(tmp)
