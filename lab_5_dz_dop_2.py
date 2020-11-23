df28b794f3fd7457875663ac5d2dda6a76c9427a
Maxim12395 <71729685+Maxim12395@users.noreply.github.com>
2020-11-23 13:23:26 +0200

    2141414141242

---
+++ lab_5_dz_dop_2.py
@@ -0,0 +1,13 @@
+from sympy import sqrt
+from sympy import symbols
+from sympy import sin, cos, pi, exp
+from sympy import simplify, expand, factor, trigsimp
+from sympy import solveset, linsolve, nonlinsolve, Eq, solve
+from sympy import Abs
+from sympy.vector import CoordSys3D
+
+N = CoordSys3D("N")
+
+a = 7*N.i + 2*N.j - 8*N.k
+b = - 4*N.i + x*N.j + 9*N.k
+
