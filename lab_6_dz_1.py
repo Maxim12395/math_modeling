423181785932cc10d760eb27e450b805d7cdf067
Maxim12395 <71729685+Maxim12395@users.noreply.github.com>
2020-11-26 11:13:40 +0200

    987876876876876

--- lab_6_dz_1.py
+++ lab_6_dz_1.py
@@ -1,18 +1,20 @@
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib as ml
+import matplotlib.pyplot as plt
 
-plt.plot([1,2,3],[5,7,10])
-plt.savefig('pic1.png')
+plt.plot([1,5,5],[1,1,5])
+plt.plot([1,1,1],[5,5,1])
 
-x = [1,5,1]
-y = [1,1,1]
+x =[1,5,5]
+y =[5,5,1]
+z =[1,1,1]
+q =[1,1,5]
 
+plt.plot(x, y, z, q, color='r', label="luchte", marker="o", ms=5)
 
-plt.plot(x,y,color='g', label="luchte", marker="o", ms=5)
-
-plt.xlabel("coord: x")
-plt.ylabel("coord: y")
 plt.title("base")
 plt.legend()
 plt.grid()
+plt.axis("equal")
+
 plt.savefig('pic1.png')
