6c8580b53bcaeec7b5b7a9becb49814a21acbb3f
Maxim12395 <71729685+Maxim12395@users.noreply.github.com>
2020-12-15 16:48:22 +0200

    876989576575474754

--- lab_7_dz_1_2.py
+++ lab_7_dz_1_2.py
@@ -1,15 +1,15 @@
 import matplotlib.pyplot as plt
+from math import sqrt, cos, sin
 import numpy as np
 
 
 def astroid(r):
-    x=[]
-    y=[]
-    for t in range (1,1000):
-        x.append((r*np.sin(t))**3)
-        y.append((r*np.cos(t))**3)
 
-    plt.scatter(x,y)
+    t = np.linspace(-2*np.pi, 6*np.pi, 100)
+    x = (r*np.sin(t)**3)
+    y = (r*np.cos(t)**3)
+    plt.axis("equal")
+    plt.plot(x,y)
     plt.show()
     
 astroid(3)
