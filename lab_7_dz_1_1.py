236be9cafb5e868ef980fc4d39cbb98a72a9ab4b
Maxim12395 <71729685+Maxim12395@users.noreply.github.com>
2020-12-15 16:26:12 +0200

    3426334746980

--- lab_7_dz_1_1.py
+++ lab_7_dz_1_1.py
@@ -1,14 +1,32 @@
+#import matplotlib.pyplot as plt
+#from math import sqrt, cos, sin
+#import numpy as np
+
+#def cicloid(r):
+#  x = []
+#  y = []
+#  for t in np.linspace(-2*np.pi, 2*np.pi, 100):
+   # x.append(r*(t - sin(t)))
+  #  y.append(r*(1 - cos(t))) 
+ #   plt.plot(x,y) 
+ #   plt.show()  
+
+#cicloid(10)
+
 import matplotlib.pyplot as plt
 from math import sqrt, cos, sin
 import numpy as np
 
-def cicloid(r):
-  x = []
-  y = []
-  for t in np.linspace(-2*np.pi, 2*np.pi, 100):
-    x.append(r*(t - sin(t)))
-    y.append(r*(1 - cos(t))) 
-    plt.plot(x,y) 
-    plt.show()  
 
+
+def cicloid(r):
+
+    t = np.linspace(-2*np.pi, 6*np.pi, 100)
+    x = (r*(t - np.sin(t)))
+    y = (r*(1 - np.cos(t))) 
+    plt.plot(x,y) 
+    plt.axis("equal")
+    plt.show()  
+    
 cicloid(10)
+
