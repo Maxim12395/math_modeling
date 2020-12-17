9e7e76b0f0d8ebf0a8f4f74b94ffd353cff8edd0
Maxim12395 <71729685+Maxim12395@users.noreply.github.com>
2020-12-17 17:47:46 +0200

    577866765767678

--- lab_7_dz_3_2.py
+++ lab_7_dz_3_2.py
@@ -1,7 +1,29 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Dec 17 17:21:06 2020
+import numpy as np
+import matplotlib.pyplot as plt
+from math import sqrt, cos, sin, pi, exp
+from matplotlib.animation import FuncAnimation
 
-@author: Home
-"""
+fig, ax = plt.subplots()
 
+anim_object, = plt.plot([], [], "-", color = "b")
+plt.axis("equal")
+xdata, ydata = [], []
+edge=20
+ax.set_xlim(-edge, edge)
+ax.set_ylim(-edge, edge)
+
+def animate(t):
+    x0 = 0
+    y0 = 0
+    xdata.append(16*np.sin(t)**3)
+    ydata.append(13*np.cos(t)- 5*np.cos(2*t) - 2*np.cos(3*t) - np.cos(4*t))
+    anim_object.set_data(xdata, ydata)
+    return anim_object
+
+ani=FuncAnimation(fig,
+                  animate,
+                  frames=np.arange(0, 25, 0.1),
+                  interval=100 
+                  )
+
+ani.save("lab_7_dz_4.gif")
