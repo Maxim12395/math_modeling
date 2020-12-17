271b450adfc5faec194d27722a4af0f3096e2f96
Maxim12395 <71729685+Maxim12395@users.noreply.github.com>
2020-12-17 16:08:21 +0200

    32452343453425

--- lab_7_dz_3_1.py
+++ lab_7_dz_3_1.py
@@ -3,14 +3,19 @@ from math import sqrt, cos, sin, pi
 import numpy as np
 
 def babochka(e):
-    
     t = np.linspace(-2*np.pi, 6*np.pi, 100)
-    x = (np.sin*(t)*((e**(np.cos*(t))) - 2*np.cos*(4*t) + (np.sin**5)*(t/12)))
-    y = (np.cos*(t)*((e**(np.cos*(t))) - 2*np.cos*(4*t) + (np.sin**5)*(t/12)))
+    # x = (np.sin*(t)*((e**(np.cos*(t))) - 2*np.cos*(4*t) + (np.sin**5)*(t/12)))
+    # y = (np.cos*(t)*((e**(np.cos*(t))) - 2*np.cos*(4*t) + (np.sin**5)*(t/12)))
     
+    for x in (-5 , 5):
+        x = ((np.sin*(t)*(e**(np.cos*(t)))) - (2*np.cos*(4*t)) + ((np.sin**5)*(t/12)))
+    for y in (-5 , 5):
+        y = (np.cos*(t)*((e**(np.cos*(t))) - 2*np.cos*(4*t) + (np.sin**5)*(t/12)))
+    for t in (0 , 12*np.pi):
+        t = np.linspace(-2*np.pi, 6*np.pi, 100)
+        
+        plt.axis("equal")
+        plt.plot(x,y)
+        plt.show()
     
-    plt.axis("equal")
-    plt.plot(y)
-    plt.show()
-    
-babochka(3)
+babochka(5)
