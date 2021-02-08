29e2053a50c33d13625d0642fef9dd39470c501f
Maxim12395 <71729685+Maxim12395@users.noreply.github.com>
2021-02-08 20:41:39 +0200

    8765783465634875

--- lab_10_dz_3.py
+++ lab_10_dz_3.py
@@ -1,16 +1,26 @@
 from scipy.integrate import odeint
 import numpy as np
 import matplotlib.pyplot as plt
+
 x=np.arange(-5,5,0.01)
-def obdolbosfunc(v,x):
-    y, omega=v
-    dy_dt=omega
-    domega_dt=np.sin(x)+np.cos(x)
-    return dy_dt, domega_dt
-y0=3
-omega0=0
-v0=y0,omega0
-sol=odeint(obdolbosfunc,v0,x)
-plt.plot(x,sol[:,1],'r',label='y(t)')
-plt.plot(x,sol[:,0],'b',label='omega(t)')
+
+def diff_func(v,x):
+
+    y, w =v
+
+    dy_dt = w
+    
+    dw_dt = np.sin(x) + np.cos(x)
+    
+    return dy_dt, dw_dt
+
+y0= 3
+
+dy_dt0= 0
+
+v0= y0, dy_dt0
+
+sol=odeint(diff_func,v0,x)
+plt.plot(x,sol[:,1],'r',label="dy_dt")
+plt.plot(x,sol[:,0],'b',label="dw_dt")
 plt.show()
