53410a774bad596126f31ac3f1750888322b4ce2
Maxim12395 <71729685+Maxim12395@users.noreply.github.com>
2021-02-09 16:14:36 +0200

    78663456385

--- lab_10_dz_4.py
+++ lab_10_dz_4.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 x = np.arange(-5, 5, 0.01) 
 
-def diff_func(v, t):
+def diff_func(v, x):
     
     y , omega  = v
     
@@ -15,10 +15,10 @@ def diff_func(v, t):
     return dy_dt , domega_dt
 
 y0 = 4
-dy_dt = - 1
+omega0 = - 1
 
 
-v0 = y0, dy_dt
+v0 = y0, omega0
 
 sol = odeint(diff_func, v0, x)  
 
@@ -26,4 +26,4 @@ plt.plot(x, sol[:, 0], "b", label = "dy_dt")
 plt.plot(x, sol[:, 1], "r", label = "domega_dt")
 
 plt.legend()
-plt.shpw()    
+plt.show()    
