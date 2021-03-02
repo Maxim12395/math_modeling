bd49a6baa30658d43c2d590779f23e529b0ebc2a
Maxim12395 <71729685+Maxim12395@users.noreply.github.com>
2021-03-02 16:49:46 +0200

    324234324234234342

---
+++ lab_11_dop_1.py
@@ -0,0 +1,47 @@
+import numpy as np
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt 
+from matplotlib.animation import FuncAnimation
+
+frames = 200
+
+t = np.linspace(0, 5, frames)
+x = np.ones(len(t))
+
+def move_func(z, t):
+    
+    a, b, c = z
+    
+    da_dt = k1 * (a0 - A - b - c)
+    
+    db_dt = k2 * (a0 - A - b - c)
+    
+    dc_dt = k3 * (a0 - A - b - c)
+    
+    return da_dt, db_dt, dc_dt
+
+k1 = 0.5
+
+k2 = 0.3
+
+k3 = 0.2
+
+a0 = 15
+
+A = 0
+
+b0 = 0
+
+c0 = 0
+
+z0 = a0, b0, c0
+
+sol = odeint(move_func, z0, t) 
+
+
+plt.plot(t, sol[:, 1], "b", label = "da_dt")
+plt.plot(t, sol[:, 0], "r", label = "db_dt")
+plt.plot(t, sol[:, 2], "k", label = "dc_dt")
+
+plt.legend()
+plt.show()    
